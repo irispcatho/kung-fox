09e7a41ca04853e3f529d3b8763253ba5b3aeb50
Iris Pecquet <iris.pecquet@lacatholille.fr>
2022-09-26 14:27:18 +0200

    Basic movement + jump

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,124 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace _Scripts
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody2D rb;
+        [SerializeField] private new Collider2D collider;
+
+        [SerializeField] private float speed;
+
+        [SerializeField]
+        private float jumpPower; //This variable will determine the initial velocity to apply when jumping.
+
+        [SerializeField] [Range(1f, 5f)]
+        private float
+            jumpFallGravityMultiplier; //This variable will determine the gravity scale applied to the Rigidbody when the player is falling after performing a jump.
+
+        [SerializeField]
+        private float
+            groundCheckHeight; //This variable will determine the height of the box (OverlapBox method) that we’ll be using below the player to determine if the it’s on the ground or not.
+
+        [SerializeField]
+        private LayerMask
+            groundMask; //This variable will allow us to check for colliders that are within the ground layer when using the OverlapBox method.
+
+        [SerializeField]
+        private float
+            disableGroundCheckTime; //This variable will determine how much time does the ground check gets disabled when jumping in order to avoid resetting the jumping bool value.
+
+        private Vector2
+            boxCenter; //This variable will indicate the central coordinate of the box that checks if the player is on the ground or not.
+
+        private Vector2 boxSize; // This variable will indicate the size (width, height) of the same box.
+        private bool groundCheckEnabled = true; // This variable will indicate if the ground check is enabled or not.
+        private float initialGravityScale; // This variable will store the initial gravity scale value of the Rigidbody.
+        private bool jumping; // This variable will indicate if the player is currently jumping.
+        private Vector2 moveInput;
+        private PlayerInputs playerInputs;
+
+        private WaitForSeconds
+            wait; // This variable will be used to wait within a coroutine before enabling the ground check again.
+
+
+        private void Awake()
+        {
+            playerInputs = new PlayerInputs();
+
+            initialGravityScale = rb.gravityScale;
+            wait = new WaitForSeconds(disableGroundCheckTime);
+            playerInputs.Player.Jump.performed += Jump;
+        }
+
+        private void FixedUpdate()
+        {
+            Move();
+            HandleGravity();
+        }
+
+        private void OnEnable()
+        {
+            playerInputs.Player.Enable();
+        }
+
+        private void OnDisable()
+        {
+            playerInputs.Player.Disable();
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = jumping ? Color.red : Color.green;
+            Gizmos.DrawWireCube(boxCenter, boxSize);
+        }
+
+        private void Jump(InputAction.CallbackContext obj)
+        {
+            if (IsGrounded())
+            {
+                rb.velocity += Vector2.up * jumpPower;
+                jumping = true;
+                StartCoroutine(EnableGroundCheckAfterJump());
+            }
+        }
+
+        private IEnumerator EnableGroundCheckAfterJump()
+        {
+            groundCheckEnabled = false;
+            yield return wait;
+            groundCheckEnabled = true;
+        }
+
+        private bool IsGrounded()
+        {
+            Bounds bounds = collider.bounds;
+            boxCenter = new Vector2(bounds.center.x, bounds.center.y) +
+                        Vector2.down * (bounds.extents.y + groundCheckHeight / 2);
+
+            boxSize = new Vector2(bounds.size.x, groundCheckHeight);
+
+            Collider2D groundBox = Physics2D.OverlapBox(boxCenter, boxSize, 0f, groundMask);
+
+            return groundBox;
+        }
+
+        private void HandleGravity()
+        {
+            if (groundCheckEnabled && IsGrounded())
+                jumping = false;
+            else if (jumping && rb.velocity.y < 0)
+                rb.gravityScale = initialGravityScale * jumpFallGravityMultiplier;
+            else
+                rb.gravityScale = initialGravityScale;
+        }
+
+        private void Move()
+        {
+            moveInput = playerInputs.Player.Move.ReadValue<Vector2>();
+            rb.velocity = new Vector2(moveInput.x * speed, rb.velocity.y);
+        }
+    }
+}
